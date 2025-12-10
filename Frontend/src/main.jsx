1dfcf9ac08336a42c84c91a141076fe24d898237
Sunny_Saurya <sunnysaurya9955@gmail.com>
2025-12-11 00:15:46 +0530

    Updated files

--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -6,13 +6,8 @@ import "leaflet/dist/leaflet.css";
 import "leaflet.markercluster/dist/MarkerCluster.css";
 import "leaflet.markercluster/dist/MarkerCluster.Default.css";
 
-
-
-
-
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
-  
   </StrictMode>,
 )
