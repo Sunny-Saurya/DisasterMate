abe8f1c54230f652a42c34d9260c35c4b46d7b96
Sunny_Saurya <sunnysaurya9955@gmail.com>
2025-12-28 00:00:23 +0530

    updated file

--- Backend/models/User.js
+++ Backend/models/User.js
@@ -15,7 +15,7 @@ const userSchema = new mongoose.Schema({
         trim: true,
         match: [/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/, 'Please provide a valid email']
     },
-    password: {
+    password: {   
         type: String,
         required: [true, 'Please provide a password'],
         minlength: 6,
